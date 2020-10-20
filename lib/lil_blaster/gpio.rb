c1db608aedff023996ccb849ccc1dbf110f7ee6e
Justin Piotroski <justin.piotroski@gmail.com>
2020-10-20 01:22:41 -0700

    Add a hook to get the wave functions

--- lib/lil_blaster/gpio.rb
+++ lib/lil_blaster/gpio.rb
@@ -4,6 +4,11 @@ module LilBlaster
   # Handles the low level interfacing with the Pi
   module GPIO
     class << self
+      # Exposes the waveform creation and execution interface
+      def wavetuner
+        connection.wave
+      end
+
       # Takes in the +pin_id+ and returns a gpio pin class
       def gpio_pin(pin_id)
         connection.gpio(pin_id)
