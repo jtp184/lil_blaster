2011348b79fe6b16c2e22bdf599f72c3e4449e60
Justin Piotroski <justin.piotroski@gmail.com>
2020-10-20 02:01:55 -0700

    Adds pin class with functions for reading, writing, and callbacks

--- lib/lil_blaster/gpio.rb
+++ lib/lil_blaster/gpio.rb
@@ -4,6 +4,11 @@ module LilBlaster
   # Handles the low level interfacing with the Pi
   module GPIO
     class << self
+      # Takes the +on_pin+, +off_pin+, and +length+ and composes a pulse
+      def pulse_converter(on_pin, off_pin, length)
+        wavetuner.pulse(on_pin, off_pin, length)
+      end
+
       # Exposes the waveform creation and execution interface
       def wavetuner
         connection.wave
@@ -34,6 +39,99 @@ module LilBlaster
       # Checks if we have Pigpio defined for use
       def pigpio_defined?
         defined?(Pigpio)
+      end
+    end
+
+    # Models the pin as an object, simplifying the interface
+    class Pin
+      # Which pin is in question
+      attr_reader :id
+      # Whether this pin is for input or output
+      attr_reader :direction
+      # The GPIO pin object to puppet
+      attr_reader :gpio_pin
+      # The currently configured callback, if any
+      attr_reader :callback
+
+      # Takes in the +pin+ number and the +dir+ symbol for direction
+      def initialize(pin, dir = :input)
+        @id = pin
+        @gpio_pin = GPIO.gpio_pin(id)
+        @direction = dir
+      end
+
+      # Sets the direction using the pin path direction file
+      def direction=(dir)
+        raise ArgumentError unless %i[input output].include?(dir)
+
+        unless direction == dir
+          @direction = dir
+          gpio_pin.mode = dir == :input ? 0 : 1
+        end
+
+        self
+      end
+
+      # Detect whether gpio_pin reads as 1
+      def on?
+        gpio_pin.read == 1
+      end
+
+      # Detect whether gpio_pin reads as 0
+      def off?
+        gpio_pin.read.zero?
+      end
+
+      # Writes a 1 to the gpio_pin
+      def turn_on
+        gpio_pin.write(1) if direction == :output
+      end
+
+      # Writes a 0 to the gpio_pin
+      def turn_off
+        gpio_pin.write(0) if direction == :output
+      end
+
+      # Waits until the pin reads #on? or +timeout+ seconds have ellapsed
+      def wait(timeout = nil)
+        if timeout.nil?
+          nil until on?
+          true
+        else
+          start_time = Time.now
+
+          timeup, pinon = nil
+
+          loop do
+            timeup = Time.now - start_time < timeout
+            pinon = on?
+
+            break if timeup || pinon
+          end
+
+          pinon
+        end
+      end
+
+      # Takes in an +edge_sym+ for whether to trigger on rising, falling, or either, and
+      # a +blk+ function to run for the callback, and commits it to an instance variable
+      def start_callback(edge_sym, &blk)
+        edge_num = case edge_sym
+                   when :rising
+                     0
+                   when :falling
+                     1
+                   when :either
+                     2
+                   end
+
+        @callback = gpio_pin.callback(edge_num, &blk)
+      end
+
+      # Stops the callback stored on the pin object
+      def stop_callback
+        @callback.cancel
+        @callback = nil
       end
     end
   end
