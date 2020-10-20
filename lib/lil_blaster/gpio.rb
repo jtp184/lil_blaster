027ef71122b4e81602063fcb225e9a820d9aa492
Justin Piotroski <justin.piotroski@gmail.com>
2020-10-20 01:15:55 -0700

    Added documentation

--- lib/lil_blaster/gpio.rb
+++ lib/lil_blaster/gpio.rb
@@ -4,10 +4,12 @@ module LilBlaster
   # Handles the low level interfacing with the Pi
   module GPIO
     class << self
+      # Memoizes and returns the hardware interface
       def connection
         @connection ||= driver.new
       end
 
+      # Determines what to use as the driver, based on #pigpio_defined?
       def driver
         if pigpio_defined?
           Pigpio
@@ -19,6 +21,7 @@ module LilBlaster
 
       private
 
+      # Checks if we have Pigpio defined for use
       def pigpio_defined?
         defined?(Pigpio)
       end
