0ae277e7eea3ed68ccae914a6d4da8ac2817b6f2
Justin Piotroski <justin.piotroski@gmail.com>
2020-10-25 15:08:56 -0700

    Added initial blaster class

--- lib/lil_blaster.rb
+++ lib/lil_blaster.rb
@@ -1,6 +1,13 @@
 require 'lil_blaster/version'
 require 'lil_blaster/gpio'
+require 'lil_blaster/blaster'
 
 # Ruby gem for interacting with Infrared transmissions on the Raspberry Pi
 module LilBlaster
+  attr_writer :transmitter_pin
+
+  # Defaults to 17
+  def transmitter_pin
+    @transmitter_pin ||= 17
+  end
 end
---
+++ lib/lil_blaster/blaster.rb
@@ -0,0 +1,19 @@
+require 'forwardable'
+
+module LilBlaster
+  # The IR Transmitter module
+  class Blaster
+    class << self
+      extend Forwardable
+
+      def_delegators :@pin, :turn_on, :turn_off, :on?, :off?
+
+      private
+
+      # The underlying GPIO pin, id determined by LilBlaster.transmitter_pin
+      def pin
+        @pin ||= GPIO::Pin.new(LilBlaster.transmitter_pin, :output)
+      end
+    end
+  end
+end
