559b2050e2ae856a969fd7a37dc1c4014f3129c7
Justin Piotroski <justin.piotroski@gmail.com>
2020-10-25 15:13:32 -0700

    Put the variables on the eigenclass properly

--- lib/lil_blaster.rb
+++ lib/lil_blaster.rb
@@ -4,10 +4,12 @@ require 'lil_blaster/blaster'
 
 # Ruby gem for interacting with Infrared transmissions on the Raspberry Pi
 module LilBlaster
-  attr_writer :transmitter_pin
+  class << self
+    attr_writer :transmitter_pin
 
-  # Defaults to 17
-  def transmitter_pin
-    @transmitter_pin ||= 17
+    # Defaults to 17
+    def transmitter_pin
+      @transmitter_pin ||= 17
+    end
   end
 end
