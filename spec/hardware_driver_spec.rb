2a318ed1d6c05c77b03648606beea099a45113d5
Justin Piotroski <justin.piotroski@gmail.com>
2020-10-25 14:17:17 -0700

    Fix fixture code for driver test

--- spec/hardware_driver_spec.rb
+++ spec/hardware_driver_spec.rb
@@ -1,15 +1,23 @@
 RSpec.describe 'Hardware driver spec' do
   it 'has a hardware driver when pigpio is defined' do
     # Fake constant to trick #defined?
-    Pigpio = 1 unless defined?(Pigpio)
+
+    delink = if defined?(Pigpio)
+               false
+             else
+               Pigpio = 1
+               true
+             end
 
     expect(LilBlaster::GPIO.driver).to be(Pigpio)
-
-    Object.send(:remove_const, :Pigpio)
+    Object.send(:remove_const, :Pigpio) if delink
   end
 
   it 'has a hardware driver when pigpio is undefined, and issues a warning' do
+    backup = defined?(Pigpio) ? pigpio : nil
     og_stderr = $stderr
+
+    Object.send(:remove_const, :Pigpio) if defined(Pigpio)
     $stderr = StringIO.new
 
     expect(LilBlaster::GPIO.driver).to be(nil)
@@ -19,5 +27,6 @@ RSpec.describe 'Hardware driver spec' do
     expect($stderr.string.chomp).to eq('Pigpio is not defined, please require it.')
 
     $stderr = og_stderr
+    Pigpio = backup
   end
 end
