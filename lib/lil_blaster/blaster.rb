f35656bfa223e4fa470a3dd78cf293e6d0097fc3
Justin Piotroski <justin.piotroski@gmail.com>
2020-10-25 15:30:11 -0700

    Remove forwardable

--- lib/lil_blaster/blaster.rb
+++ lib/lil_blaster/blaster.rb
@@ -1,11 +1,8 @@
-require 'forwardable'
-
 module LilBlaster
   # The IR Transmitter module
   class Blaster
     class << self
-      extend Forwardable
-
+      # The basic methods available from the pin
       %i[turn_on turn_off on? off?].each do |symbol|
         define_method(symbol) do
           val = pin.send(symbol)
