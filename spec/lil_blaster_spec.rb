d9208d992575b6bb5520d170cdf0d7c0d659806b
Justin Piotroski <justin.piotroski@gmail.com>
2020-10-27 15:43:45 -0700

    Add the low hanging fruit constant updating spec

--- spec/lil_blaster_spec.rb
+++ spec/lil_blaster_spec.rb
@@ -13,5 +13,15 @@ RSpec.describe LilBlaster do
       expect(LilBlaster.transmitter_pin).to eq(69)
       LilBlaster.transmitter_pin = og
     end
+
+    it 'can read and write the reader pin' do
+      expect(LilBlaster.reader_pin).not_to be_nil
+
+      og = LilBlaster.reader_pin
+
+      LilBlaster.reader_pin = 69
+      expect(LilBlaster.reader_pin).to eq(69)
+      LilBlaster.reader_pin = og
+    end
   end
 end
