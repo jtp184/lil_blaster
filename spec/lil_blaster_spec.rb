e4048bb169bc8a7737eaa4429c14772deef9a1cf
Justin Piotroski <justin.piotroski@gmail.com>
2020-10-26 23:54:15 -0700

    Hotfix to make the constant test work

--- spec/lil_blaster_spec.rb
+++ spec/lil_blaster_spec.rb
@@ -7,8 +7,11 @@ RSpec.describe LilBlaster do
     it 'can read and write the transmitter pin' do
       expect(LilBlaster.transmitter_pin).not_to be_nil
 
+      og = LilBlaster.transmitter_pin
+
       LilBlaster.transmitter_pin = 69
       expect(LilBlaster.transmitter_pin).to eq(69)
+      LilBlaster.transmitter_pin = og
     end
   end
 end
