7abb7a8ec625efdf6ec7a8f0a30996caf01d5254
Justin Piotroski <justin.piotroski@gmail.com>
2020-10-31 17:14:06 -0700

    Test the blinking

--- spec/blaster_spec.rb
+++ spec/blaster_spec.rb
@@ -3,4 +3,18 @@ RSpec.describe LilBlaster::Blaster, :hardware do
     code = LilBlaster::Transmission.new(data: [517, 1732, 517, 609])
     LilBlaster::Blaster.transmit(code)
   end
+
+  it 'can perform basic blinking' do
+    LilBlaster::Blaster.turn_on
+
+    expect(LilBlaster::Blaster.on?).to eq(true)
+
+    LilBlaster::Blaster.turn_off
+    
+    expect(LilBlaster::Blaster.off?).to eq(true)
+  end
+
+  it 'can check its active status' do
+    expect(LilBlaster::Blaster.on?).not_to eq(LilBlaster::Blaster.off?)
+  end
 end
