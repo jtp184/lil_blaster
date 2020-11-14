0ee1530907f82e0cf32ba7c2630503316c701444
Justin Piotroski <justin.piotroski@gmail.com>
2020-11-14 14:25:33 -0800

    Add a metaprogramming method to shorten commmand definition

--- lib/lil_blaster/cli.rb
+++ lib/lil_blaster/cli.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'thor'
+require 'tty-config'
+require 'strings-case'
 
 module LilBlaster
   # Handle the application command line parsing
@@ -8,8 +10,42 @@ module LilBlaster
   #
   # @api public
   class CLI < Thor
+    class << self
+      def def_command(args = {}, &blk)
+        cmd = args.fetch(:cmd)
+
+        sym = cmd.to_sym
+        str = cmd.to_str
+        cons = Strings::Case.pascalcase(str).to_sym
+
+        desc str, args.fetch(:desc)
+
+        args.fetch(:method_options, []).each do |mop|
+          method_option(*mop)
+        end
+
+        define_method(sym) do
+          if options[:help]
+            invoke :help, [str]
+          else
+            require_relative "commands/#{str}"
+
+            blk.call if block_given?
+
+            LilBlaster::Commands.const_get(cons)
+                                .new(options)
+                                .execute
+          end
+        end
+      end
+    end
+
     # Error raised by this runner
     Error = Class.new(StandardError)
+
+    def self.exit_on_failure?
+      true
+    end
 
     desc 'version', 'lil_blaster version'
 
