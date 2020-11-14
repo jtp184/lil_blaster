667ff5efdd10363f6dd0c5fa0c2282bc910a62a9
Justin Piotroski <justin.piotroski@gmail.com>
2020-11-14 15:37:10 -0800

    Added basic functionality to config command

--- lib/lil_blaster/command.rb
+++ lib/lil_blaster/command.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'forwardable'
+require 'strings-case'
 
 module LilBlaster
   # A Command runner under the CLI
--- lib/lil_blaster/commands/config.rb
+++ lib/lil_blaster/commands/config.rb
@@ -5,7 +5,44 @@ module LilBlaster
     # Initial tool setup
     class Config < LilBlaster::Command
       def execute(input: $stdin, output: $stdout)
-        puts @options
+        config_file.write(force: true) unless file_exist?
+
+        if @options.key?(:interactive)
+          interactive
+        elsif @options.key?(:get)
+          puts config_file.fetch(@options[:get])
+        elsif @options.key?(:set)
+          set_config_file_option
+          puts @options[:set].join(' => ')
+        end
+      end
+
+      def interactive
+        config_file.to_h.each do |key, value|
+          resp = prompt.ask("#{Strings::Case.titlecase key}: ", default: value)
+          config_file.set(key, value: resp)
+        end
+
+        config_file.write(force: true)
+      end
+
+      private
+
+      def set_config_file_option
+        config_file.set(
+          @options[:set][0],
+          value: @options[:set][1]
+        )
+
+        config_file.write(force: true)
+      end
+
+      def file_exist?
+        LilBlaster::CLI.config_file_exist?
+      end
+
+      def config_file
+        LilBlaster::CLI.config_file
       end
     end
   end
