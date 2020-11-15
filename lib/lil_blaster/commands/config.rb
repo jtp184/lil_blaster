93529107ddf583ada166bd2c45ffe38c16cca025
Justin Piotroski <justin.piotroski@gmail.com>
2020-11-14 16:16:37 -0800

    Remove module prefix

--- lib/lil_blaster/commands/config.rb
+++ lib/lil_blaster/commands/config.rb
@@ -5,28 +5,28 @@ module LilBlaster
     # Initial tool setup
     class Config < LilBlaster::Command
       def execute(_input: $stdin, _output: $stdout)
-        LilBlaster::ConfigFile.save unless LilBlaster::ConfigFile.exist?
+        ConfigFile.save unless ConfigFile.exist?
 
         if @options.key?(:interactive)
-          LilBlaster::ConfigFile.each do |key, value|
+          ConfigFile.each do |key, value|
             resp = prompt.ask("#{Strings::Case.titlecase key}: ", default: value)
-            LilBlaster::ConfigFile[key] = resp
+            ConfigFile[key] = resp
           end
         elsif @options.key?(:get) && %w[get all].include?(@options[:get])
-          LilBlaster::ConfigFile.each do |tuple|
+          ConfigFile.each do |tuple|
             puts tuple.join(' => ')
           end
         elsif @options.key?(:get)
-          resp = LilBlaster::ConfigFile[@options[:get]]
+          resp = ConfigFile[@options[:get]]
 
           puts resp
         elsif @options.key?(:set)
-          LilBlaster::ConfigFile[@options[:set][0]] = @options[:set][1]
+          ConfigFile[@options[:set][0]] = @options[:set][1]
 
           puts @options[:set].join(' => ')
         end
 
-        LilBlaster::ConfigFile.save unless @options.key?(:get)
+        ConfigFile.save unless @options.key?(:get)
       end
     end
   end
