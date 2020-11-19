cf55578ac1f15f32afe359ed0e0720c10c3d8cdc
Justin Piotroski <justin.piotroski@gmail.com>
2020-11-18 20:34:53 -0800

    Ditch the metaprogramming instead of maintaining a britle wrapper to save lines

--- lib/lil_blaster/cli.rb
+++ lib/lil_blaster/cli.rb
@@ -10,35 +10,6 @@ module LilBlaster
   #
   # @api public
   class CLI < Thor
-    class << self
-      # Metaprogramming method, takes in +args+ and simplifies defining a CLI command
-      def def_command(args = {})
-        cmd = args.fetch(:cmd)
-
-        sym = cmd.to_sym
-        str = cmd.to_str
-        cons = Strings::Case.pascalcase(str).to_sym
-
-        desc str, args.fetch(:desc)
-
-        args.fetch(:method_options, []).each do |mop|
-          method_option(*mop)
-        end
-
-        define_method(sym) do
-          if options[:help]
-            invoke :help, [str]
-          else
-            require_relative "commands/#{str}"
-
-            LilBlaster::Commands.const_get(cons)
-                                .new(options)
-                                .execute
-          end
-        end
-      end
-    end
-
     # Error raised by this runner
     Error = Class.new(StandardError)
 
@@ -59,27 +30,21 @@ module LilBlaster
 
     map %w[--version -v] => :version
 
-    def_command(
-      cmd: 'config',
-      desc: 'Initialize, set, and get configuration options',
-      method_options: [
-        [
-          :interactive,
-          { aliases: '-i', type: :boolean, desc: 'Edit settings with an interactive prompt' }
-        ],
-        [
-          :set,
-          { aliases: '-s', type: :array, desc: 'Set a configuration value' }
-        ],
-        [
-          :get,
-          { aliases: '-g', type: :string, desc: 'Get the current value of a config setting' }
-        ],
-        [
-          :unset,
-          { aliases: '-u', type: :string, desc: 'Unset a configuration value' }
-        ]
-      ]
-    )
+    desc 'config', 'Initialize, set, and get configuration options'
+    method_option :interactive, aliases: '-i', type: :boolean, desc: 'Edit settings with an interactive prompt'
+    method_option :set, aliases: '-s', type: :array, desc: 'Set a configuration value'
+    method_option :get, aliases: '-g', type: :string, desc: 'Get the current value of a config setting'
+    method_option :unset, aliases: '-u', type: :string, desc: 'Unset a configuration value'
+
+    # Initialize, set, and get configuration options
+    def config
+      if options[:help]
+        invoke :help, [:config]
+      else
+        require_relative 'commands/config'
+
+        LilBlaster::Commands::Config.new(options).execute
+      end
+    end
   end
 end
