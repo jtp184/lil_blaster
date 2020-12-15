9d107e3db6311f9a8330629450af2de6db133eda
Justin Piotroski <justin.piotroski@gmail.com>
2020-12-15 01:05:07 -0800

    Return the current codex, whether it exists or is new

--- lib/lil_blaster/commands/learn.rb
+++ lib/lil_blaster/commands/learn.rb
@@ -6,6 +6,20 @@ module LilBlaster
     class Learn
       def execute(_input: $stdin, _output: $stdout)
       end
+
+      private
+
+      def current_codex
+        return @current_codex if @current_codex
+
+        srch = @options[:codex]
+
+        matched = LilBlaster::Codex.autoload.find do |cdx|
+          cdx.remote_name.downcase =~ /#{srch}/
+        end
+
+        @current_codex = matched || LilBlaster::Codex.new(remote_name: srch)
+      end
     end
   end
 end
