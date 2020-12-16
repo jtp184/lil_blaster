e0d96a7c12b9494c6e20ad357501a814dfeefc67
Justin Piotroski <justin.piotroski@gmail.com>
2020-12-15 23:55:33 -0800

    Move sleep to top of function

--- lib/lil_blaster/commands/learn.rb
+++ lib/lil_blaster/commands/learn.rb
@@ -51,9 +51,8 @@ module LilBlaster
 
       # Given a +sym+ it captures a transmission and decodes it, then adds to the current codex
       def learn_new_symbol(sym)
-        print "Ready to capture `#{pastel.yellow(sym.to_s)}`"
         sleep 2
-        puts
+        puts "Ready to capture `#{pastel.yellow(sym.to_s)}`"
         puts 'Please press and hold the key'
 
         burst = LilBlaster::Reader.record(first: 4)
