6c400d4af246b3814bae1e429016c69f5264677a
Justin Piotroski <justin.piotroski@gmail.com>
2020-12-16 17:26:21 -0800

    Change default edge argument

--- lib/lil_blaster/buttons.rb
+++ lib/lil_blaster/buttons.rb
@@ -99,7 +99,7 @@ module LilBlaster
       def start_callback(idx, args = {}, &blk)
         callbacks[idx] = args[:function] || blk
 
-        pins[idx].start_callback(args.fetch(:callback_edge, :either), &callbacks[idx])
+        pins[idx].start_callback(args.fetch(:callback_edge, :rising), &callbacks[idx])
         callbacks[idx]
       end
 
@@ -107,7 +107,7 @@ module LilBlaster
       def resume_callback(idx, args = {})
         raise ArgumentError, 'No current callback' unless callbacks[idx]
 
-        pins[idx].start_callback(args.fetch(:callback_edge, :either), &callbacks[idx])
+        pins[idx].start_callback(args.fetch(:callback_edge, :rising), &callbacks[idx])
       end
 
       # Stops the callback running for button +idx+
